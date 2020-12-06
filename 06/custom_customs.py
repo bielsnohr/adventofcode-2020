989f4dd875bc3ee2d38d1f6b3a97604524671ff6
Matthew Bluteau <matthew.bluteau@gmail.com>
2020-12-06 14:35:14 +0000

    Solve puzzle 2 of day 6

--- 06/custom_customs.py
+++ 06/custom_customs.py
@@ -3,9 +3,14 @@ import sys
 #import re
 
 def count_yes_in_group(group_answers):
-    yes_set = set()
+    yes_set = set(''.join(group_answers))
+    return len(yes_set)
+
+
+def count_intersect_yes_in_group(group_answers):
+    yes_set = set(''.join(group_answers))
     for answer in group_answers:
-        yes_set = yes_set | set(answer)
+        yes_set = yes_set & set(answer)
     return len(yes_set)
 
 
@@ -26,8 +31,27 @@ def collect_total_yes_count(input_file):
     return count_yes
 
 
+def collect_intersect_yes_count(input_file):
+    group_answers = []
+    count_yes = 0
+    with open(input_file) as input:
+        while True:
+            line = input.readline()
+            if line == '' or line == '\n':
+                count_yes += count_intersect_yes_in_group(group_answers)
+                group_answers = []
+                if line == '':
+                    break
+            else:
+                group_answers.append(line.strip())
+
+    return count_yes
+
+
 def main(input_file='input.txt'):
-    print(collect_total_yes_count(input_file))
+    print('Total union yes count: ', collect_total_yes_count(input_file))
+    print('Total intersect yes count: ',
+          collect_intersect_yes_count(input_file))
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
--- 06/test_custom_customs.py
+++ 06/test_custom_customs.py
@@ -1,5 +1,7 @@
 import pytest
 from custom_customs import count_yes_in_group, collect_total_yes_count
+from custom_customs import count_intersect_yes_in_group 
+from custom_customs import collect_intersect_yes_count
 
 
 @pytest.mark.parametrize("group_answers, expected_yes_count",[
@@ -10,5 +12,17 @@ def test_count_yes_in_group(group_answers, expected_yes_count):
     assert count_yes_in_group(group_answers) == expected_yes_count
 
 
+@pytest.mark.parametrize("group_answers, expected_yes_count",[
+    (['abcx', 'abcy', 'abcz'], 3), (['ab', 'ac'], 1), 
+    (['a', 'a', 'a', 'a'], 1)
+])
+def test_count_intersect_yes_in_group(group_answers, expected_yes_count):
+    assert count_intersect_yes_in_group(group_answers) == expected_yes_count
+
+
 def test_collect_total_yes_count():
     assert collect_total_yes_count(input_file='test_input.txt') == 11
+
+
+def test_collect_intersect_yes_count():
+    assert collect_intersect_yes_count(input_file='test_input.txt') == 6
